84aeee2f6a618a9a186f55b6bd29c91a7e697813
wiltonsaturnino <116773668+WiltonSaturnino200890@users.noreply.github.com>
2025-04-26 13:45:25 -0300

    Adicionar arquivos base do projeto
    Arquivos iniciais

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem-vindo ao TCC Escola App!</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f5f5f5',
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+});
